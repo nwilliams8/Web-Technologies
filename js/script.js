5f436825f2b4b9781507622cd7e2b9c63c1cd58d
nwilliams8 <33866591+nwilliams8@users.noreply.github.com>
2018-03-13 20:47:36 +0000

    Change 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    const button = document.getElementById("button");
+    button.addEventListener('click', closeNav)
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    const button = document.getElementById("button");
+    button.addEventListener('click', openNav)
+}
 window.onload = function() {
   currentYear();
 };
